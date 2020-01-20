d0f1a0051ab47f066204c983661fa055dd398a56
Samuel Taylor <samtaylor0497@hotmail.com>
2020-01-20 21:46:05 +0000

    scaffolded identity pages back in to project

---
+++ syslogSite/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using syslogSite.Data;
+
+[assembly: HostingStartup(typeof(syslogSite.Areas.Identity.IdentityHostingStartup))]
+namespace syslogSite.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}

54bb3c16055ded90448786b290ca23513ee5fdb5
Samuel Taylor <samtaylor0497@hotmail.com>
2020-01-23 17:54:03 +0000

    Changed environment for listener (testing purposes). Added 4 sections to the index page and a brief rundown of alert metrics updating every 10 seconds

--- syslogSite/Pages/Index.cshtml.cs
+++ syslogSite/Pages/Index.cshtml.cs
@@ -10,12 +10,17 @@ using Microsoft.Extensions.WebEncoders.Testing;
 using SyslogShared.Models;
 using SyslogShared;
 using syslogSite.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using Newtonsoft.Json;
 
 namespace syslogSite.Pages
 {
     public class IndexModel : PageModel
     {
-        private ApplicationDbContext _context;
+        public string AlertTotal { get; set; }
+        private ApplicationDbContext _context = GetContext();
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -25,7 +30,42 @@ namespace syslogSite.Pages
 
         public void OnGet()
         {
-             
+            AlertTotal = _context.alerts.Count().ToString();
+
+        }
+
+        public JsonResult OnGetUpdateMetrics()
+        {
+            List<string> ErrorNames = new List<string>
+                {"Emergency", "Alert", "Critical", "Error", "Warning", "Notification", "Information", "Debug"};
+            Dictionary<string,string> counts = new Dictionary<string, string>();
+            for (int i = 0; i < 8; i++)
+            {
+                counts.Add(ErrorNames[i], _context.alerts.Count(a => a.Severity == i).ToString());
+            }
+
+            return new JsonResult(JsonConvert.SerializeObject(counts));
+        }
+        public static ApplicationDbContext GetContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseMySql(
+                GetConfig().GetConnectionString("DefaultConnection"),
+                mySqlOptions =>
+                {
+                    mySqlOptions.ServerVersion(new Version(8, 0, 17), ServerType.MySql);
+                    mySqlOptions.MigrationsAssembly("syslogSite");
+                });
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+        private static IConfiguration GetConfig()
+        {
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{environmentName}.json", true, true)
+                .AddEnvironmentVariables();
+            return builder.Build();
         }
     }
 }

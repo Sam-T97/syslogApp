21cb0e6a1bb17afa6bf0560257c3a76ae44df653
Samuel Taylor <samtaylor0497@hotmail.com>
2020-02-23 13:05:33 +0000

    Added a test page to test configuration rollback functionality

---
+++ syslogSite/Pages/ConfigRollbackTest.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Renci.SshNet;
+
+namespace syslogSite.Pages
+{
+    public class ConfigRollbackTestModel : PageModel
+    {
+        private readonly SyslogShared.ApplicationDbContext _context;
+
+        public void OnGet()
+        {
+
+        }
+
+        public JsonResult OnGetConfigs()
+        {
+            SshClient client = new SshClient("TestPI","pi","test");
+            client.Connect();
+            SshCommand cmd = client.CreateCommand("./ListConfigs.py");
+            cmd.Execute();
+            List<string> configs = cmd.Result.Split(new[] {"\n"}, StringSplitOptions.None).ToList();
+            client.Disconnect();
+            return new JsonResult(configs);
+        }
+    }
+}

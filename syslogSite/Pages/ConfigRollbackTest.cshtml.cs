890d7f18840358ca6df6c599722b99ecce10f695
Samuel Taylor <samtaylor0497@hotmail.com>
2020-02-23 13:55:25 +0000

    Made a test page that can rollback configurations of devices to the specified file.

--- syslogSite/Pages/ConfigRollbackTest.cshtml.cs
+++ syslogSite/Pages/ConfigRollbackTest.cshtml.cs
@@ -28,7 +28,33 @@ namespace syslogSite.Pages
             cmd.Execute();
             List<string> configs = cmd.Result.Split(new[] {"\n"}, StringSplitOptions.None).ToList();
             client.Disconnect();
+            client.Dispose();
             return new JsonResult(configs);
+        }
+
+        public JsonResult OnGetRollback(string configName)
+        {
+            try
+            {
+                var client = new SshClient("TestPI", "pi", "test");
+                client.Connect();
+                var cmd = client.CreateCommand("./Restore.py " + configName);
+                cmd.Execute();
+                client.Disconnect();
+                client.Dispose();
+                return new JsonResult(new
+                {
+                    result = "success"
+                });
+            }
+            catch (Exception e)
+            {
+                return new JsonResult(new
+                {
+                    result = "Failed",
+                    error = e.Message
+                });
+            }
         }
     }
 }

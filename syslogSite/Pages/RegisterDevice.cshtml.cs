f307e4d1953473a056890d6008915cc9f8e77c45
Samuel Taylor <samtaylor0497@hotmail.com>
2020-02-25 17:38:15 +0000

    Added additional navigation options to layout page. Also added checking against DB when adding a device to ensure no host or IP conflicts. Added heartbeat check on deivce registration with escape if device unreachable

--- syslogSite/Pages/RegisterDevice.cshtml.cs
+++ syslogSite/Pages/RegisterDevice.cshtml.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using SyslogShared;
 using SyslogShared.Models;
+using System.Net.NetworkInformation;
 
 namespace syslogSite.Pages
 {
     public class RegisterDeviceModel : PageModel
     {
+        public static string feedback;
         private readonly SyslogShared.ApplicationDbContext _context;
 
         public RegisterDeviceModel(SyslogShared.ApplicationDbContext context)
@@ -36,9 +39,35 @@ namespace syslogSite.Pages
                 return Page();
             }
 
-            _context.Devices.Add(Device);
-            await _context.SaveChangesAsync();
+            var check = _context.Devices.Any(i => i.IP == Device.IP || i.HostName == Device.HostName);
+            if (check == true)
+            {
+                feedback = "A device already exists in the system with either: the same Hostname or IP address. \n" +
+                           "Please check the devices in the system against the data you wish to register and try again.";
+                return Page();
+            }
 
+            try
+            {
+                Ping testPing = new Ping();
+                PingReply reply = testPing.Send(Device.IP, 2000);
+                if (reply.Status != IPStatus.Success)
+                {
+                    feedback = "The server could not reach the IP address you specified. \n" +
+                               "Make sure the device you're trying to add is alive and can reach this server";
+                    return Page();
+                }
+
+                _context.Devices.Add(Device);
+                await _context.SaveChangesAsync();
+                
+            }
+            catch (Exception e)
+            {
+                feedback = "An error occured trying to register the device \n" +
+                           e.Message;
+                return Page();
+            }
             return RedirectToPage("./Index");
         }
     }

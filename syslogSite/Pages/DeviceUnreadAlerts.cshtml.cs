3b8d56a4395bb3f937577c4d502d97d23a7915da
Samuel Taylor <samtaylor0497@hotmail.com>
2020-02-26 12:06:52 +0000

    Added page to list devices with outstanding alerts. Added page to navigation drop down menu

---
+++ syslogSite/Pages/DeviceUnreadAlerts.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SyslogShared;
+using SyslogShared.Models;
+
+namespace syslogSite.Pages
+{
+    public class DeviceUnreadAlertsModel : PageModel
+    {
+        private readonly SyslogShared.ApplicationDbContext _context;
+
+        public DeviceUnreadAlertsModel(SyslogShared.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Device> Device { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Device = await _context.Devices.ToListAsync();
+        }
+    }
+}

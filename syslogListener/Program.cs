4bc1a36c9d54f0f63ea2b86d844023c944c431d3
Samuel Taylor <samtaylor0497@hotmail.com>
2020-02-27 13:37:58 +0000

    Added a heartbeating thread to the listener and adjusted the email send code. Requires testing.

--- syslogListener/Program.cs
+++ syslogListener/Program.cs
@@ -10,9 +10,13 @@ using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using System.Text.RegularExpressions;
 using System.Net.Mail;
 using System.Net;
+using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using SyslogShared;
+using System.Net.NetworkInformation;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 
 namespace syslogListener
 {
@@ -33,6 +37,9 @@ namespace syslogListener
             Thread writeMessage = new Thread(MessageHandler);
             writeMessage.Start();
             writeMessage.Join();
+            Thread heartBeatThread = new Thread(HeartBeat);
+            heartBeatThread.Start();
+            heartBeatThread.Join();
         }
 
         private static void Server_MessageReceived(object sender, MessageReceivedEventArgs e)
@@ -83,16 +90,67 @@ namespace syslogListener
             }
         }
 
-        private static void EmailAlert(SyslogMessage m)
+        private static void HeartBeat()
+        {
+            var dbContext = GetContext();
+            int interval;
+            int.TryParse(dbContext.appvars.Where(a => a.VariableName == "HBInterval")
+                .Select(a => a.Value)
+                .FirstOrDefault(),out interval);
+            while (true)
+            {
+                try
+                {
+                    List<Device> devices = new List<Device>();
+                    foreach (Device d in dbContext.Devices)
+                    {
+                        Ping testPing = new Ping();
+                        PingReply reply = testPing.Send(d.IP,2000);
+                        if (reply.Status == IPStatus.Success) {continue; }
+                        devices.Add(d);
+                    }
+
+                    if (devices.Count != 0)
+                    {
+                        Task.Run(() => EmailAlert(null, devices));
+                    }
+
+                    Thread.Sleep((int)TimeSpan.FromMinutes(interval).TotalMilliseconds);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Something went wrong with the Heart beater");
+                    Console.WriteLine(e.Message);
+                    Thread.Sleep((int) TimeSpan.FromMinutes(interval).TotalMilliseconds);
+                }
+            }
+        }
+        private static void EmailAlert([Optional]SyslogMessage m, [Optional] List<Device> device)
         {
             using var message = new MailMessage();
-            message.To.Add("");//TODO get emails from db
-            message.From = new MailAddress("syslogsnapper@gmail.com", "SyslogSnapper");
-            message.Subject = "A high priority alert has been received from " + m.RemoteEndPoint.Address;
-            message.Body = "The details are as follows: <br/> Received: " + m.Received
-                                                                       + "<br/> Facility: " + m.Facility
-                                                                       + "<br/> Severity: " + m.Severity
-                                                                       + "<br/> Full Message: " + m.Text;
+            if (m != null)
+            {
+                message.To.Add(""); //TODO get emails from db
+                message.From = new MailAddress("syslogsnapper@gmail.com", "SyslogSnapper");
+                message.Subject = "A high priority alert has been received from " + m.RemoteEndPoint.Address;
+                message.Body = "The details are as follows: <br/> Received: " + m.Received
+                                                                              + "<br/> Facility: " + m.Facility
+                                                                              + "<br/> Severity: " + m.Severity
+                                                                              + "<br/> Full Message: " + m.Text;
+            }
+            else
+            {
+                StringBuilder hostnamesBuilder = new StringBuilder();
+                foreach (Device d in device)
+                {
+                    hostnamesBuilder.AppendLine(d.HostName);
+                }
+                message.To.Add(""); //TODO get emails from db
+                message.From = new MailAddress("syslogsnapper@gmail.com", "SyslogSnapper");
+                message.Subject = "Device(s) have failed to heartbeat";
+                message.Body = "The non-responsive devices are: <br/> " + hostnamesBuilder;
+            }
+
             message.IsBodyHtml = true;
             try
             {

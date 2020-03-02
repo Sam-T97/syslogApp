67de92751f15e1268a1dfcea0567599bf799d9f1
Samuel Taylor <samtaylor0497@hotmail.com>
2020-03-02 16:42:50 +0000

    Added device details page and linked to ListDevices page. Added 3 tabs for details, config rollback and terminal access to device

---
+++ syslogSite/Pages/DeviceDetails.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SyslogShared;
+using SyslogShared.Models;
+
+namespace syslogSite.Pages
+{
+    public class DeviceDetailsModel : PageModel
+    {
+        private readonly SyslogShared.ApplicationDbContext _context;
+
+        public DeviceDetailsModel(SyslogShared.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Device Device { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Device = await _context.Devices.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Device == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}

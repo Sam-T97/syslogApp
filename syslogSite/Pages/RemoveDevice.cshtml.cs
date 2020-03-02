8e1b78fb1a81a4a6eba39e041104da3058f07b22
Samuel Taylor <samtaylor0497@hotmail.com>
2020-03-02 13:37:40 +0000

    Finished page for removing a device from the system. Tested with random device and functionality is as expected

--- syslogSite/Pages/RemoveDevice.cshtml.cs
+++ syslogSite/Pages/RemoveDevice.cshtml.cs
@@ -40,7 +40,7 @@ namespace syslogSite.Pages
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync(int? id)
+        public async Task<IActionResult> OnPostDeleteAsync(int? id)
         {
             if (id == null)
             {
@@ -53,20 +53,20 @@ namespace syslogSite.Pages
             {
                 _context.Devices.Remove(Device);
                 await _context.SaveChangesAsync();
+                feedback = "Device removed from the system";
             }
 
-            return RedirectToPage("./Index");
+            return RedirectToPage("./RemoveDevice");
         }
 
-        public async Task<IActionResult> OnGetLocateAsync(int? id, string? host)
+        public async Task<IActionResult> OnPostLocateAsync(string? ip, string? hostname)
         {
-            Device = _context.Devices.FirstOrDefault(m => m.ID == id || m.HostName == host);
+            Device = await _context.Devices.FirstOrDefaultAsync(m => m.IP == ip || m.HostName == hostname);
             if (Device == null)
             {
                 feedback = "No device found with these details. Check the details are correct and try again";
                 return Page();
             }
-
             return Page();
         }
     }
